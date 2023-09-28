1046fec7c9bb885cc81872626bc3fcbc61e81401
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-09-27 20:54:21 -0700

    Updated_enemy_movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,12 +12,47 @@ public class Enemy : MonoBehaviour{
 
     [Header("Movement parameters")]
     [SerializeField] private float speed;
+    private Vector3 initScale;
+    private bool movingLeft;
+
+    [Header("Idle Behavior")]
+    [SerializeField]private float idleDuration;
+    private float idleTimer;
+
+    private void Awake(){
+        initScale = enemy.localScale;
+    }
 
     private void Update(){
-        MoveInDirection(1);
+        if(movingLeft){
+            if(enemy.position.x >= leftEdge.position.x){
+                MoveInDirection(-1);
+            }else{
+                DirectionChange();
+            }
+        }else{
+            if(enemy.position.x <= rightEdge.position.x){
+                MoveInDirection(1);
+            }else{
+                DirectionChange();
+            }
+        }
+    }
+
+    private void DirectionChange(){
+
+        idleTimer += Time.deltaTime;
+
+        if(idleTimer > idleDuration){
+            movingLeft = !movingLeft;
+        }
     }
 
     private void MoveInDirection(int _direction){
+        idleTimer=0;
+
+        enemy.localScale = new Vector3(Mathf.Abs(initScale.x)*_direction,initScale.y,initScale.z);
+
         enemy.position = new Vector3(enemy.position.x + Time.deltaTime *_direction*speed,enemy.position.y,enemy.position.z);
     }
 }
