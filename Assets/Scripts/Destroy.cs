bfd30bfd80f912a0c5999dd2fc17c2acba2bb7cf
shashwot.niraula1990@gmail.com <niraulashashwot@gmail.com>
2023-10-03 03:07:26 -0700

    added background and point collection music

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Destroy : MonoBehaviour{
-    public void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.CompareTag("Player")){
+public class Destroy : MonoBehaviour
+{
+    public AudioSource audioSource; // Reference to the AudioSource component
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Play the audio clip
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+
+            // Destroy the game object
             Destroy(this.gameObject);
         }
     }
-
 }
